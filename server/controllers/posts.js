9c091b4155d9445800ac88cc39bc8d065b2b55c9
Nguyễn Thành đạt <thanhdat262000@gmail.com>
2021-01-30 16:21:28 +0700

    update-route-patch

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,6 @@
 import { error } from "console";
 import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
   try {
@@ -18,4 +19,14 @@ export const createPost = async (req, res) => {
   } catch {
     res.status(409).json({ message: error.message });
   }
+};
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (mongoose.Types.ObjectId.isValid(_id))
+    return res.status.send("Cant find that id");
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+  res.json(updatedPost);
 };
