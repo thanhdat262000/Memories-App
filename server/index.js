1033d18ebb5b78dea8b1db3b7163f94ec40392cb
Nguyễn Thành đạt <thanhdat262000@gmail.com>
2021-02-01 22:22:08 +0700

    changes-for-deployment

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome to my website!");
+});
 
 const PORT = process.env.PORT || 5000;
 mongoose
